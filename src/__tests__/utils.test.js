a65015dc07ab3ec1dacdf0792b52a01088e4f44b
william lee <will.lee.npc@gmail.com>
2024-02-02 22:17:04 -0500

    complete lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,21 @@
 // Your tests here
+import {pointsForWord} from "../utils";
+
+describe("pointsForWord", ()=>{
+    it("calculates the total points for a word (1 point per vowel, 2 per consonant)", ()=>{
+        const arg = "test";
+
+        const result = pointsForWord(arg);
+        
+        expect(result).toBe(7);
+    });
+
+    it("handles uppercase and lowercase input", ()=>{
+        const arg = "tEsT";
+
+        const result = pointsForWord(arg);
+
+        expect(result).toBe(7);
+    });
+})
+
